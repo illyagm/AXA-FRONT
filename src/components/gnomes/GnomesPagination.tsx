09946d8da080aa96482d0d37ca83eab5ddaef328
noobaso <39062812+noobaso@users.noreply.github.com>
2019-12-03 21:11:57 +0100

    Pagination Fix

--- src/components/gnomes/GnomesPagination.tsx
+++ src/components/gnomes/GnomesPagination.tsx
@@ -1,94 +1,3 @@
-/*
-import React from 'react'
-import { Grid, makeStyles, Paper } from '@material-ui/core';
-
-const GnomesPagination = ( gnomesPerPage:any, filteredGnomes: any, paginate:any ) => {
-
-    const pageNumbers = [];
-    const useStyles = makeStyles(theme => ({
-        root: {
-          flexGrow: 1,
-        },
-        paper: {
-          padding: theme.spacing(1),
-          textAlign: 'center',
-          color: theme.palette.text.secondary,
-        },
-      }));
-    for(let i = 1; i <= Math.ceil(gnomesPerPage.filteredGnomes / gnomesPerPage.gnomesPerPage); i++){
-        pageNumbers.push(i);
-    }
-    console.log(gnomesPerPage.filteredGnomes);
-    const classes = useStyles();
-    return (
-      <div className={classes.root}>
-      <Grid container spacing={1}>
-              {pageNumbers.map(number => (
-                  <Grid item xs={1}>
-                  <Paper className={classes.paper} key={number} onClick={() => gnomesPerPage.paginate(number)}>
-                      
-                          {number}
- 
-                  </Paper>
-                  </Grid>
-              ))}
-      </Grid> 
-      </div>
-  )
-  
-  }
-  
-  export default GnomesPagination;
-  */
-  /*
-
-
-    return (
-      return (
-        <div className={classes.root}>
-        <Grid container spacing={1}>
-
-        <Grid item xs={1}>
-        <Paper disabled={currentNumber === 1} className={classes.paper} onClick={() => gnomesPerPage.paginate(currentNumber-1)}>
-                <Icon icon={'arrow-left'}/>
-        </Paper>
-        </Grid>
-                    <Grid item xs={1}>
-                    <Paper className={classes.paper} onClick={() => gnomesPerPage.paginate(currentNumber)}>
-                            {currentNumber}
-                    </Paper>
-                    </Grid>
-                    <Grid item xs={1}>
-                    <Paper className={classes.paper} onClick={() => gnomesPerPage.paginate(currentNumber+1)}>
-                            {currentNumber+1}
-                    </Paper>
-                    </Grid>
-                    <Grid item xs={1}>
-                    <Paper className={classes.paper} onClick={() => gnomesPerPage.paginate(currentNumber+2)}>
-                            {currentNumber+2}
-                    </Paper>
-                    </Grid>
-                    <Grid item xs={1}>
-                    <Paper disabled={currentNumber === pageNumbers.length} className={classes.paper} onClick={() => gnomesPerPage.paginate(currentNumber+1)}>
-                            <Icon icon={'arrow-right'}/>
-                    </Paper>
-                    </Grid>
-        </Grid> 
-        </div>
-    )
-
-
-    )
-
-  */
-  
-  /* 
-  
-  const [currentNumber, setCurrentNumber] = useState(1);
-  
-  */
-
-
 import React, { useState } from 'react'
 import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
 import Pagination from "material-ui-flat-pagination";
@@ -99,9 +8,9 @@ const GnomesPagination = ( gnomesPerPage:any, filteredGnomes: any, paginate:any 
     const [offsetPage, setOffset] = useState(6);
     const theme = createMuiTheme();
 
-    const handleClick = (offsetPage:any, gnomesPerPage:any) => {
+    const handleClick = (offsetPage:any, gnomesPerPage:any, page:number) => {
         setOffset(offsetPage);
-        //gnomesPerPage.paginate(offsetPage);
+        gnomesPerPage.paginate(page);
       }
 
 
@@ -113,7 +22,7 @@ const GnomesPagination = ( gnomesPerPage:any, filteredGnomes: any, paginate:any 
           limit={gnomesPerPage.gnomesPerPage}
           offset={offsetPage}
           total={gnomesPerPage.filteredGnomes}
-          onClick={(e, offset) => handleClick(offset, gnomesPerPage)}
+          onClick={(e, offset, page:number) => handleClick(offset, gnomesPerPage, page)}
           id='2'
         />
       </MuiThemeProvider>
