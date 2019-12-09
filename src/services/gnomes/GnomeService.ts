3bfd73e669c43ff90cf9a49c6d90b41af3f9af59
noobaso <39062812+noobaso@users.noreply.github.com>
2019-12-09 22:36:01 +0100

    Code Cleaning

--- src/services/gnomes/GnomeService.ts
+++ src/services/gnomes/GnomeService.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 export default class GnomeService implements IGnomeService{
     public async getAll(): Promise<any> {
-        //devuelvo la promise de axios y la resolveré en el componente dónde llame la funcion getAll()
         return axios.get("https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json"); 
     }       
 }
